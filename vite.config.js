65e5e337a6e56dea06d05b74891e6c6a3e883569
Mahathi Gumudavelli <101474281+mgjslearn@users.noreply.github.com>
2025-01-28 22:26:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  base: '/mahathi_portfolio/',  // Add this line and replace [repo-name] with your actual repo name
 })
